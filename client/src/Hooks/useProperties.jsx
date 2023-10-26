24fcf80311624309113e169ddea926a2482d6341
Mayuri13 <mayuri.goswami2017@gmail.com>
2023-10-26 10:17:58 +0530

    Updated

---
+++ client/src/Hooks/useProperties.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const useProperties = () => {
+  return (
+    <div>useProperties</div>
+  )
+}
--- client/src/Hooks/useProperty.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const useProperty = () => {
-  return (
-    <div>useProperty</div>
-  )
-}
